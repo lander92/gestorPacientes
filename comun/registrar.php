5a80ad2dff2a3ec36e1754b75d964c039861414f
unknown <landerreyes92@gmail.com>
2014-10-15 12:34:32 +0200

    carpeta comun

---
+++ comun/registrar.php
@@ -0,0 +1,30 @@
+<?php
+if(!empty($_POST['dni'])){
+	//incluimos el archivo conexion.php para utilizar sus funciones
+	include '../conexion.php';
+	//guardo en variables el nombre de la bd y de la tabla que voy a utilizar
+	$base="gestorPacientes";
+	$dbTabla="usuarios";
+	$conexion=conectar();
+	//guardo en variables los datos que recojo del formulario
+	$dni=$_POST['dni'];
+	$nombre=$_POST['nombre'];
+	$apellidos=$_POST['apellidos'];
+	$localidad=$_POST['localidad'];
+	$telefono=$_POST['telefono'];
+	$password=$_POST['password'];
+	$tipo=$_POST['tipoPersona'];
+	//guardo en $consulta la consulta que voy a ejecutar
+	$consulta="INSERT INTO $dbTabla (dni,nombre,apellidos,password,localidad,telefono,tipo) 
+			VALUES ('$dni','$nombre','$apellidos','$password','$localidad','$telefono','$tipo')";
+	//ejecutamos la consulta
+	if(mysql_db_query($base, $consulta, $conexion)){
+		//si se ejecuta correctamente
+		header ("location: index.php");
+	}else{
+		//si no se ejecuta correctamente
+		print "<h3>Registro  no añadido</h3> <br>";
+	}
+}else{
+	print "<h3>DNI Obligatorio</h3>";
+}
---
+++ comun/validar.php
@@ -0,0 +1,75 @@
+<?php
+/****************************************
+**establecemos conexion con el servidor.
+**nombre del servidor: localhost.
+**Nombre de usuario: root.
+**Contraseña de usuario: root.
+**Si la conexion fallara mandamos un msj 'ha fallado la conexion'**/
+mysql_connect('localhost','root','')or die ('Ha fallado la conexión: '.mysql_error());
+
+/*Luego hacemos la conexión a la base de datos. 
+**De igual manera mandamos un msj si hay algun error*/
+mysql_select_db('gestorPacientes')or die ('Error al seleccionar la Base de Datos: '.mysql_error());
+ 
+/*caturamos nuestros datos que fueron enviados desde el formulario mediante el metodo POST
+**y los almacenamos en variables.*/
+$dni = $_POST["dni"];   
+$password = $_POST["password"];
+
+/*Consulta de mysql con la que indicamos que necesitamos que seleccione
+**solo los campos que tenga como nombre_administrador el que el formulario
+**le ha enviado*/
+$result = mysql_query("SELECT * FROM usuarios WHERE dni = '$dni'");
+
+//Validamos si el nombre del administrador existe en la base de datos o es correcto
+if($row = mysql_fetch_array($result)){     
+//Si el usuario es correcto ahora validamos su contraseña
+ if($row["password"] == $password){
+ 	switch ($row["tipo"]){
+ 		case "medico":
+ 			//Creamos sesión
+	 		session_start();  
+	  		//Almacenamos el nombre de usuario en una variable de sesión usuario
+	  		$_SESSION['dni'] = $dni;  
+	  		$_SESSION['tipo']=$tipo; 
+	  		//Redireccionamos a la pagina: index.php
+	 		 header("Location: ../medico/indexMedico.php");
+	 		 break;
+
+ 		case "paciente":
+ 			//Creamos sesión
+	 		session_start();  
+	  		//Almacenamos el nombre de usuario en una variable de sesión usuario
+	  		$_SESSION['dni'] = $dni; 
+	  		$_SESSION['tipo']=$tipo; 
+	  		//Redireccionamos a la pagina: index.php
+	 		 header("Location: ../paciente/indexPacientes.php");  
+	 		 break;
+ 	}
+ }else{
+  //En caso que la contraseña sea incorrecta enviamos un msj y redireccionamos a login.php
+  ?>
+   <script languaje="javascript">
+    alert("Contraseña Incorrecta");
+    location.href = "index.php";
+   </script>
+  <?php          
+ }
+}else{
+ //en caso que el nombre de administrador es incorrecto enviamos un msj y redireccionamos a login.php
+?>
+ <script languaje="javascript">
+  alert("El nombre de usuario es incorrecto!");
+  location.href = "index.php";
+ </script>
+<?php           
+}
+
+//Mysql_free_result() se usa para liberar la memoria empleada al realizar una consulta
+mysql_free_result($result);
+
+/*Mysql_close() se usa para cerrar la conexión a la Base de datos y es 
+**necesario hacerlo para no sobrecargar al servidor, bueno en el caso de
+**programar una aplicación que tendrá muchas visitas ;) .*/
+mysql_close();
+?>
