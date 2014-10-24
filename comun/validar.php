50906739e12b762ec340390b8800eb45e1734902
unknown <landerreyes92@gmail.com>
2014-10-24 12:43:05 +0200

    cambios validar

--- comun/validar.php
+++ comun/validar.php
@@ -21,40 +21,40 @@ $result = mysql_query("SELECT * FROM usuarios WHERE dni = '$dni'");
 //Validamos si el dni existe en la base de datos o es correcto
 if($row = mysql_fetch_array($result)){     
 //Si el dni es correcto ahora validamos su contraseña
- if($row["password"] == $password){
- 	//si la contraseña es correcta
- 	//miramos el tipo
- 	switch ($row["tipo"]){
- 		//en el caso de que sea medico
- 		case "medico":
- 			//Creamos sesión
-	 		session_start();  
-	  		//Almacenamos el dni y tipo en una variable de sesión
-	  		$_SESSION['dni'] = $dni;  
-	  		$_SESSION['tipo']=$tipo; 
-	  		//Redireccionamos a la pagina: indexMedico.php
-	 		 header("Location: ../medico/indexMedico.php");
-	 		 break;
-
- 		case "paciente":
- 			//Creamos sesión
-	 		session_start();  
-	  		//Almacenamos el dni y tipo en una variable de sesión
-	  		$_SESSION['dni'] = $dni; 
-	  		$_SESSION['tipo']=$tipo; 
-	  		//Redireccionamos a la pagina: indexPacientes.php
-	 		 header("Location: ../paciente/indexPacientes.php");  
-	 		 break;
- 	}
- }else{
-  //En caso que la contraseña sea incorrecta enviamos un msj y redireccionamos a index.php
+	if($row["password"] == $password){
+	 	//si la contraseña es correcta
+	 	//miramos el tipo
+	 	switch ($row["tipo"]){
+	 		//en el caso de que sea medico
+	 		case "medico":
+	 			//Creamos sesión
+		 		session_start();  
+		  		//Almacenamos el dni y tipo en una variable de sesión
+		  		$_SESSION['dni'] = $dni;  
+		  		$_SESSION['tipo']=$tipo; 
+		  		//Redireccionamos a la pagina: indexMedico.php
+		 		 header("Location: ../medico/indexMedico.php");
+		 		 break;
+	
+	 		case "paciente":
+	 			//Creamos sesión
+		 		session_start();  
+		  		//Almacenamos el dni y tipo en una variable de sesión
+		  		$_SESSION['dni'] = $dni; 
+		  		$_SESSION['tipo']=$tipo; 
+		  		//Redireccionamos a la pagina: indexPacientes.php
+		 		 header("Location: ../paciente/indexPacientes.php");  
+		 		 break;
+	 	}
+	 }else{
+  	//En caso que la contraseña sea incorrecta enviamos un msj y redireccionamos a index.php
   ?>
    <script languaje="javascript">
     alert("Contraseña Incorrecta");
     location.href = "index.php";
    </script>
   <?php    
- }
+	}
 }else{
  //en caso que el nombre de administrador es incorrecto enviamos un msj y redireccionamos a login.php
 ?>
