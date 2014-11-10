6a8a8b90f25327b284a0ccf5311f7938e9ef34fa
unknown <landerreyes92@gmail.com>
2014-11-10 08:31:20 +0100

    delete modal

--- index.php
+++ index.php
@@ -46,24 +46,6 @@
 			    <div class="col-sm-offset-6 col-sm-6">
 			      <input id="submit_form" type="submit" class="btn btn-primary btn-lg btn-block" name="entrar" value="entrar"/>
 			    </div>
-			    <!--inicio ventana modal -->
-			<div class="modal fade" id="modal">
-			  <div class="modal-dialog">
-			    <div class="modal-content">
-			      <div class="modal-header">
-			        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-			        <h4 class="modal-title" id="titModal">Error Usuario</h4>
-			      </div>
-			      <div class="modal-body" >
-			        <p id="bodyModal">Falta nombre usuario&hellip;</p>
-			      </div>
-			      <div class="modal-footer">
-			        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-			      </div>
-			    </div><!-- /.modal-content -->
-			  </div><!-- /.modal-dialog -->
-			</div><!-- /.modal -->
-		<!-- fin ventna modal -->
 			</form><!--end formulario-->
 		</div><!--row del formulario+logo-->
 		<div class="row">
