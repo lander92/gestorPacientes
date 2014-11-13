b31a5412c05ffae3642d37494a3f463b212bcedb
unknown <reyeslander92@gmail.com>
2014-11-13 12:37:01 +0100

    cambio bd

--- gestorpacientes.sql
+++ gestorpacientes.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 4.0.10.4
 -- http://www.phpmyadmin.net
 --
--- Servidor: localhost
--- Tiempo de generación: 24-10-2014 a las 10:31:50
--- Versión del servidor: 5.5.24-log
--- Versión de PHP: 5.3.13
+-- Servidor: 127.2.118.2:3306
+-- Tiempo de generación: 12-11-2014 a las 08:06:05
+-- Versión del servidor: 5.5.37
+-- Versión de PHP: 5.3.3
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `gestorpacientes`
 --
-CREATE DATABASE `gestorpacientes` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `gestorpacientes`;
 
 -- --------------------------------------------------------
 
@@ -34,10 +32,10 @@ CREATE TABLE IF NOT EXISTS `pacientes` (
   `nombre` varchar(20) NOT NULL,
   `apellidos` varchar(40) NOT NULL,
   `localidad` varchar(20) NOT NULL,
-  `telefono` varchar(12) NOT NULL,
+  `telefono` varchar(9) NOT NULL,
   `historial` varchar(250) NOT NULL,
   PRIMARY KEY (`dni_medico`,`dni_paciente`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -51,17 +49,27 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `apellidos` varchar(40) NOT NULL,
   `password` varchar(12) NOT NULL,
   `localidad` varchar(20) NOT NULL,
-  `telefono` varchar(12) NOT NULL,
+  `telefono` varchar(9) NOT NULL,
   `tipo` varchar(10) NOT NULL,
   PRIMARY KEY (`dni`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`dni`, `nombre`, `apellidos`, `password`, `localidad`, `telefono`, `tipo`) VALUES
-('12345678R', 'r', 'r', 'r', 'r', 'r', 'medico');
+('12345678R', 'r', 'r', 'rrrrrr', 'r', '111111111', 'medico');
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  ADD CONSTRAINT `pacientes_ibfk_1` FOREIGN KEY (`dni_medico`) REFERENCES `usuarios` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
